d9b53782377f04a7d383154169434c5a9e675b86
115409-ux <115409@estudante.se.df.gov.br>
2026-06-13 21:50:06 -0300

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,3 +9,21 @@ if ($hora < 12) {
     echo "<p>Boa tarde!</p>";
 }
 ?>
+<h1>Testando switch com PHP</h1>
+
+<?php
+$dia = "terça";"terça";
+
+switch ($dia) {
+    case "terça";
+        echo "<p>Hoje é dia de programar!</p>";
+        break;
+
+    case "terça":
+        echo "<p>Dia de banco de dados!</p>";
+        break;
+
+    default:
+        echo "<p>Dia de estudar novos conceitos.</p>";
+}
+?>
