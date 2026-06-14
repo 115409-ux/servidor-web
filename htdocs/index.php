3f8a1c69bac07fcb037e1aae3fdc910105333300
115409-ux <115409@estudante.se.df.gov.br>
2026-06-13 21:47:21 -0300

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olá Mundo em PHP</title>
-</head>
-<body>
-    <h1><?php echo "Olá Mundo"; ?></h1>
-</body>
-</html>
+<h1>Testando if ... else com PHP</h1>
+
+<?php
+$hora = 14;
+
+if ($hora < 12) {
+    echo "<p>Bom dia!</p>";
+} else {
+    echo "<p>Boa tarde!</p>";
+}
+?>
