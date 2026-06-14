0b21f2a8682bc7f52ab169047c655a32c294efe0
115409-ux <115409@estudante.se.df.gov.br>
2026-06-13 21:56:07 -0300

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,29 +1,54 @@
-<h1>Testando if ... else com PHP</h1>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Atividade PHP</title>
+</head>
+<body>
 
-<?php
-$hora = 10;
+    <h1>Testando if ... else com PHP</h1>
 
-if ($hora < 12) {
-    echo "<p>Bom dia!</p>";
-} else {
-    echo "<p>Boa tarde!</p>";
-}
-?>
-<h1>Testando switch com PHP</h1>
+    <?php
+    $hora = 14;
 
-<?php
-$dia = "terça";"terça";
+    if ($hora < 12) {
+        echo "<p>Bom dia!</p>";
+    } else {
+        echo "<p>Boa tarde!</p>";
+    }
+    ?>
 
-switch ($dia) {
-    case "terça";
-        echo "<p>Hoje é dia de programar!</p>";
-        break;
+    <h1>Testando switch com PHP</h1>
 
-    case "terça":
-        echo "<p>Dia de banco de dados!</p>";
-        break;
+    <?php
+    $dia = "segunda";
 
-    default:
-        echo "<p>Dia de estudar novos conceitos.</p>";
-}
-?>
+    switch ($dia) {
+        case "segunda":
+            echo "<p>Hoje é dia de programar!</p>";
+            break;
+
+        case "terça":
+            echo "<p>Dia de banco de dados!</p>";
+            break;
+
+        default:
+            echo "<p>Dia de estudar novos conceitos.</p>";
+    }
+    ?>
+
+    <h1>Testando while com PHP</h1>
+
+    <ul>
+    <?php
+    $contador = 1;
+
+    while ($contador <= 5) {
+        echo "<li>Item de teste número $contador</li>";
+        $contador++;
+    }
+    ?>
+    </ul>
+
+</body>
+</html>
